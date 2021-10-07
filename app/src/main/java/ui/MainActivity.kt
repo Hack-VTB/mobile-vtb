eb4f8cc9df6ed2184fbb94716e213ac615d01492
MIshkaV <mavorozhtsov_1@edu.hse.ru>
2021-10-07 17:32:38 +0300

    test screen

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -3,11 +3,23 @@ package ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mobilevtb.R
 import com.example.mobilevtb.ui.theme.MobileVTBTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +28,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MobileVTBTheme {
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    beginScreen()
                 }
             }
         }
@@ -24,14 +36,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun beginScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.payment_solid),
+            contentDescription = "Payment solid",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .weight(6f)
+                .fillMaxWidth()
+        )
+        Text(
+            text = "Повышение финансовой грамотности - в наше время важная хуйня",
+            textAlign = TextAlign.Center,
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .weight(3f)
+                .padding(10.dp)
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .weight(1f)
+                .padding(20.dp)
+                .fillMaxWidth()
+        ) {
+            Text("Skip")
+            Text("Next")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MobileVTBTheme {
-        Greeting("Android")
+        beginScreen()
     }
 }
