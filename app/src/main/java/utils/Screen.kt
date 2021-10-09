5df771f9bf43499278c9a6d4ba38fae1386ba215
aaismailov <aaismailov@miem.hse.ru>
2021-10-09 19:07:44 +0300

    Lesson1

--- app/src/main/java/utils/Screen.kt
+++ app/src/main/java/utils/Screen.kt
@@ -6,4 +6,5 @@ sealed class Screen(val route: String) {
     object BeginScreen3: Screen("begin_screen_3")
     object StartScreen: Screen("start_screen")
     object RoutesScreen: Screen("route_screen")
+    object Lesson1Screen2: Screen("lesson_1_screen_2")
 }
